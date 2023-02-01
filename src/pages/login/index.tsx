a3b221092fcacf87e39548666b0c17cb690f87b0
白明宇 <baimingyu@4paradigm.com>
2023-02-01 18:24:09 +0800

    feat: 测试代码

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -63,7 +63,7 @@ function Index() {
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit" className="w-full">
-            登陆777
+            登陆7779
           </Button>
         </Form.Item>
       </Form>
